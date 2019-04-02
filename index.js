9c684f25c5b507f1a73430a512fa81615c3262b8
x-zen <jdharris.x@gmail.com>
2019-04-02 11:35:47 -0500

    All post endpoint functional

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const postRoutes = require('./posts/postRoutes'); // import the post endpoints
 
 const server = express(); // creates the server
 
+server.use(express.json()); // add this to make POST and PUT work
+
+
 // handle request to the root of the API
 server.get('/', (req, res) => {
-  res.send('Hello from the Posts API');
+  res.send('Hello from the Posts Challenge API');
 });
 
 // assigns the route for post endpoints
@@ -14,5 +17,5 @@ server.use('/api/posts', postRoutes);
 
 // watch for connections on port 3000
 server.listen(3000, () =>
-  console.log('\n** Server running on http://localhost:3000 **');
+  console.log('\n** Server running on http://localhost:3000 **')
 );
--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -1,31 +1,93 @@
 /* --- Routes for '/api/posts' --- */
 const express = require('express'); // import the express package
 
+const db = require('../data/db.js'); // import data helpers
+
 const router = express.Router(); // creates the route
+
 
 // POST - Creates a post using info sent in request body
 router.post('/', (req, res) => {
- res.status().json();
+  const { title, contents } = req.body;
+  const post =  { title, contents }
+  if (!title || !contents) {
+    res.status(400).json({ error: 'Please provide title and contents for the post.' })
+  }
+  db
+  .insert(post)
+  .then(postID => {
+    res.status(201).json(postID);
+  })
+  .catch(err => {
+    res.status(500).json({ error: 'There was an error while saving the post to the database.' });
+  })
 })
 
 // GET - Returns an array of all posts in database
 router.get('/', (req, res) => {
- res.status().json();
+  db
+  .find()
+  .then(posts => {
+    res.status(200).json(posts);
+  })
+  .catch(err => {
+    res.status(500).json({ error: 'The posts information could not be retrieved.' });
+  })
 })
 
 // GET - Returns post with the specified ID
 router.get('/:id', (req, res) => {
- res.status().json();
+  const { id } = req.params;
+  db
+  .findById(id)
+  .then(post => {
+    if (post.length === 0) {
+      res.status(404).json({ error: 'The post with the specified ID does not exist.'});
+      return;
+    }
+    res.status(200).json(post);
+  })
+  .catch(err => {
+    res.status(500).json({ error: 'The post information could not be retrieved.'});
+  })
 })
 
 // DELETE - Deletes post with specified ID & returns deleted post
 router.delete('/:id', (req, res) => {
- res.status().json();
+  const { id } = req.params;
+  db
+  .remove(id)
+  .then(deleted => {
+    if (deleted === 0) {
+      res.status(404).json({ error: 'The post with the specified ID does not exist.'});
+      return;
+    }
+    res.status(204).end();
+  })
+  .catch(err => {
+    res.status(500).json({ error: 'The post could not be removed.' });
+  })
 })
 
 // PUT - Updates post with specified ID & returns the moded post
 router.put('/:id', (req, res) => {
- res.status().json();
+  const { id } = req.params;
+  const { title, contents } = req.body;
+  const updates =  { title, contents };
+  if (!title || !contents) {
+    res.status(400).json({ error: 'Please provide title and contents for the post.' });
+  }
+  db
+  .update(id, updates)
+  .then(count => {
+    if (count > 0) {
+      res.status(200).json(updates);
+    }
+    res.status(404).json({ error: 'The post with the specified ID does not exist.'});
+  })
+  .catch(err => {
+    res.status(500).json({ error: 'The post information could not be modified.' });
+  })
 })
 
 
