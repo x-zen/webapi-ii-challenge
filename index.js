6fcf002c9352cdac6285556d27fa9a118d20c5c0
x-zen <jdharris.x@gmail.com>
2019-04-02 10:21:01 -0500

    Comments mapped out

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const express = require('express');
+const express = require('express'); // import the express package
 
-const postRoutes = require('./posts/postRoutes');
+const postRoutes = require('./posts/postRoutes'); // import the post endpoints
 
-const server = express();
+const server = express(); // creates the server
 
-server.use('/api/posts', postRoutes);
-
+// handle request to the root of the API
 server.get('/', (req, res) => {
   res.send('Hello from the Posts API');
 });
 
+// assigns the route for post endpoints
+server.use('/api/posts', postRoutes);
+
+// watch for connections on port 3000
 server.listen(3000, () =>
   console.log('\n** Server running on http://localhost:3000 **');
 );
--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -1,27 +1,32 @@
 /* --- Routes for '/api/posts' --- */
-const express = require('express');
+const express = require('express'); // import the express package
 
-const router = express.Router();
+const router = express.Router(); // creates the route
 
+// POST - Creates a post using info sent in request body
 router.post('/', (req, res) => {
  res.status().json();
 })
 
+// GET - Returns an array of all posts in database
 router.get('/', (req, res) => {
  res.status().json();
 })
 
+// GET - Returns post with the specified ID
 router.get('/:id', (req, res) => {
  res.status().json();
 })
 
+// DELETE - Deletes post with specified ID & returns deleted post
 router.delete('/:id', (req, res) => {
  res.status().json();
 })
 
+// PUT - Updates post with specified ID & returns the moded post
 router.put('/:id', (req, res) => {
  res.status().json();
 })
 
 
-module.exports = router; //Leave as the last line
+module.exports = router; // exports the route
